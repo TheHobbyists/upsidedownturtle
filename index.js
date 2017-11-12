e58dff6e96ff3d0a0fdb4fc877d7b5db8056c178
Austinthatsit <33360589+Austinthatsit@users.noreply.github.com>
2017-11-12 17:52:19 -0600

    fixed merge

--- index.js
+++ index.js
@@ -9,16 +9,13 @@ var array = ["one", 2 , "three"];
 var variable = "this NOT is a variable";
 var dick = false;
 
-<<<<<<< HEAD
 console.log("hello".inverse);
 console.log("rainbow".rainbow.inverse);
 console.log("trap".trap);
 console.log("ERROR".red.inverse);
-=======
 if (dick == true){
 	console.log("OHHHHHHHH YEAHHHHHHH".yellow)
 }
 if (dick == false){
 	console.log("God damn it. No dick.".red)
 }
->>>>>>> dcd1bef661b5070cc8be03460c8062b9ef19974f
