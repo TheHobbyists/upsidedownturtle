d2e907f7b6552d3f17cbd647b0c98d2fb41d2607
Austinthatsit <33360589+Austinthatsit@users.noreply.github.com>
2017-11-12 16:45:17 -0600

    added package

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+let colors = require("colors");
+
 var variable = "this NOT is a variable"
 let constant = "this NOT is a constant"
 var array = ["one", 2 , "three"];
 
-console.log(array);
+console.log(array.grey);
 
 console.log(array[1]);
