d3e12a4e2c3bd7d552ad2b9f2ab46d2073bfdd66
Austinthatsit <33360589+Austinthatsit@users.noreply.github.com>
2017-11-12 16:32:10 -0600

    added variables and constants

---
+++ index.js
@@ -0,0 +1,9 @@
+var variable = "this is a variable"
+let constant = "this is a constant"
+
+console.log(variable);
+
+variable = "still a variable "
+
+console.log(variable);
+console.log(constant);
